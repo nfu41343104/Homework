6e16c7ae6cdda8748c7078b5d3088e171845d66b
nfu41343104 <1029nfustudy@gmail.com>
2026-03-11 15:57:59 +0800

    Update preorder.cpp

--- tree練習/src/preorder.cpp
+++ tree練習/src/preorder.cpp
@@ -1,13 +1,4 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
 #include <iostream>
-#include <random>
-#include <sstream>
-#include <string>
 using namespace std;
 
 struct Node {
